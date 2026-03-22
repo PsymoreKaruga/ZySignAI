04f835b1f0b9b6529ac08809ad5f116f3f6a2a6a
PsymoreKaruga <simonkaruga945@gmail.com>
2026-03-22 03:07:12 +0300

    Fix build.sh — clean file only

--- backend/build.sh
+++ backend/build.sh
@@ -1,19 +1,5 @@
-
-
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -o errexit
-
-# Install Python dependencies
 pip install -r requirements.txt
-
-# Run database migrations
 python manage.py migrate
-
-# Collect static files
 python manage.py collectstatic --no-input
-```
-
-Add these to `backend/requirements.txt`:
-```
-pydub==0.25.1
-av==13.1.0
